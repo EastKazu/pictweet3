9ccabea689a09e6e6ba977b881e9a58e03c4fb16
EastKazu <fhornest55@gmail.com>
2024-10-10 11:03:44 +0900

    ツイートの情報取得をbefore actionにまとめる

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,5 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:show, :edit, :update, :destroy]
 
   def index
     @tweets = Tweet.all
@@ -14,27 +15,28 @@ class TweetsController < ApplicationController
   end
 
   def destroy
-    tweet = Tweet.find(params[:id])
     tweet.destroy
     redirect_to action: :index
   end
 
   def edit
-    @tweet = Tweet.find(params[:id])
   end
 
   def update
-    tweet = Tweet.find(params[:id])
     tweet.update(tweet_params)
     redirect_to action: :index
   end
 
   def show
-    @tweet = Tweet.find(params[:id])
   end
 
   private
   def tweet_params
     params.require(:tweet).permit(:name, :text, :image)
   end
+
+def set_tweet
+  @tweet = Tweet.find(params[:id])
+end
+
 end
